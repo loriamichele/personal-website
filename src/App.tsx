327426ad47c1de0878ea60727e93916c0c18b7c0
Michele Loria <info@micheleloria.net>
2020-05-23 03:42:30 +0200

    Clean up CRA stuff, add Motion lib & draft splash animations 💫

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { motion } from "framer-motion";
+import "./App.css";
+
+interface TextAccordionProps {
+  delay: number;
+  chars: string;
+}
+
+function TextAccordion({ delay, chars }: TextAccordionProps) {
+  return (
+    <>
+      {chars.split("").map((c, idx) => (
+        <motion.span
+          key={idx}
+          transition={{ delay: (idx + delay) / 13, from: 0 }}
+          animate={{ maxWidth: "9vw" }}
+        >
+          {c}
+        </motion.span>
+      ))}
+    </>
+  );
+}
 
 function App() {
+  const vh = window.innerHeight;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="viewport-center">
+        <motion.div transition={{ delay: 1.8 }} animate={{ y: vh * -0.6 }}>
+          <motion.div
+            className="dangling-text"
+            transition={{ from: 0 }}
+            animate={{ scale: 1 }}
+          >
+            <span>M</span>
+            <TextAccordion delay={4} chars="ichele" />
+            <span>&nbsp;L</span>
+            <TextAccordion delay={12} chars="oria" />
+          </motion.div>
+        </motion.div>
+      </div>
+      <motion.div
+        className="header"
+        transition={{ delay: 2.3 }}
+        animate={{ opacity: [0, 1], x: [-1000, 0] }}
+      >
+        <h1>&nbsp;</h1>
+      </motion.div>
+      <motion.h1
+        className="logo"
+        transition={{ delay: 2.3 }}
+        animate={{ y: 100 }}
+      >
+        ML
+      </motion.h1>
+    </>
   );
 }
 
